9730ad9610314f4aa6212d5151a6d913d41ced29
root <root@dx2-1.(none)>
2016-08-08 19:49:46 +0800

    Change the jvm version to jdk8.

--- installation-tools/master/init_cluster.sh
+++ installation-tools/master/init_cluster.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-mkdir /data/dfs_name
+mkdir -p /data/dfs_name
 chown hdfs /data/dfs_name
 
 for host in `cat h.slaves`; do
-  ssh $host mkdir /data/dfs_data
+  ssh $host mkdir -p /data/dfs_data
   ssh $host chown hdfs /data/dfs_data
-  ssh $host mkdir /data/mapred_local
+  ssh $host mkdir -p /data/mapred_local
   ssh $host chown mapred /data/mapred_local
-  ssh $host mkdir /data/nm_local
+  ssh $host mkdir -p /data/nm_local
   ssh $host chown yarn /data/nm_local
 done
 
 for host in `cat h.secondarynamenode`; do
-  ssh $host mkdir /data/dfs_secondname
+  ssh $host mkdir -p /data/dfs_secondname
   ssh $host chown hdfs /data/dfs_secondname
 done
 
--- installation-tools/master/install-hadoop-hbase.sh
+++ installation-tools/master/install-hadoop-hbase.sh
@@ -2,7 +2,7 @@
 
 for host in `cat h.slaves`; do
  #ssh $host yum install hadoop hadoop-yarn-nodemanager hadoop-hdfs-datanode hadoop-mapreduce  hbase-regionserver oracle-j2sdk1.7-1.7.0+update45-1.x86_64 hadoop-lzo -y
- ssh $host yum install hadoop hadoop-yarn-nodemanager hadoop-hdfs-datanode hadoop-mapreduce  hbase-regionserver java-1.7.0-openjdk-devel.x86_64 hadoop-lzo -y
+ ssh $host yum install hadoop hadoop-yarn-nodemanager hadoop-hdfs-datanode hadoop-mapreduce  hbase-regionserver java-1.8.0-openjdk-devel.x86_64 hadoop-lzo -y
 done
 
 for host in `cat h.master`; do
